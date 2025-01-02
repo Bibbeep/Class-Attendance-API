cb0b38d81a971e48ab03aa6b8a89e1e10482e8bf
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-01-03 00:02:50 +0700

    added unit tests for verifyotp auth model

--- tests/units/auth.unit.test.js
+++ tests/units/auth.unit.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-const { register } = require('../../models/auth');
+const { register, verifyOTP } = require('../../models/auth');
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcrypt');
 const speakeasy = require('speakeasy');
@@ -73,9 +73,13 @@ describe('Authentication Unit Tests', () => {
                 first_name: data.first_name,
                 last_name: data.last_name,
             });
+
+            expect(returnData).toHaveProperty('otp');
+            expect(typeof returnData.otp).toBe('string');
+            expect(isNaN(returnData.otp)).toBeFalsy();
         });
 
-        it('should throw an error', async () => {
+        it('should throw an error if email is already registered', async () => {
             const data = {
                 email: 'test1@mail.com',
                 password: 'testpassword',
@@ -88,6 +92,123 @@ describe('Authentication Unit Tests', () => {
                 new HttpRequestError(409, 'Resource conflict', [
                     {
                         message: 'email is already registered',
+                        context: {
+                            key: 'email',
+                            value: data.email,
+                        },
+                    },
+                ]),
+            );
+        });
+    });
+
+    describe('Verify OTP Model Tests', () => {
+        it('should return user data and verifies the new user', async () => {
+            const registerData = {
+                email: 'test2@mail.com',
+                password: 'testpassword',
+                first_name: 'John',
+                last_name: 'Doe',
+                birth_date: new Date('2000-01-01'),
+            };
+
+            const userData = await register(registerData);
+
+            const data = {
+                email: userData.user.email,
+                otp: userData.otp,
+            };
+
+            const returnData = await verifyOTP(data);
+
+            expect(returnData).toHaveProperty('user');
+            expect(returnData.user).toHaveProperty('id');
+            expect(returnData.user).toHaveProperty('email');
+            expect(returnData.user).toHaveProperty('first_name');
+            expect(returnData.user).toHaveProperty('last_name');
+
+            expect(typeof returnData.user.id).toBe('number');
+            expect(typeof returnData.user.email).toBe('string');
+            expect(typeof returnData.user.first_name).toBe('string');
+
+            if (returnData.user.last_name) {
+                expect(typeof returnData.user.last_name).toBe('string');
+            }
+
+            const user = await prisma.user.findUnique({
+                where: {
+                    id: userData.user.id,
+                },
+            });
+
+            expect(user.isVerified).toBe(true);
+
+            expect(returnData.user).toMatchObject({
+                id: userData.user.id,
+                email: userData.user.email,
+                first_name: userData.user.first_name,
+                last_name: userData.user.last_name,
+            });
+        });
+
+        it('should throw an error if otp is invalid', async () => {
+            const registerData = {
+                email: 'test2@mail.com',
+                password: 'testpassword',
+                first_name: 'John',
+                last_name: 'Doe',
+                birth_date: new Date('2000-01-01'),
+            };
+
+            const userData = await register(registerData);
+
+            const data = {
+                email: userData.user.email,
+                otp: '999999',
+            };
+
+            await expect(verifyOTP(data)).rejects.toThrow(
+                new HttpRequestError(400, 'Request body validation error', [
+                    {
+                        message: 'Invalid or expired otp',
+                        context: {
+                            key: 'otp',
+                            value: data.otp,
+                        },
+                    },
+                ]),
+            );
+        });
+
+        it('should throw an error if email is not registered', async () => {
+            const data = {
+                email: 'unregistered@mail.com',
+                otp: '999999',
+            };
+
+            await expect(verifyOTP(data)).rejects.toThrow(
+                new HttpRequestError(400, 'Request body validation error', [
+                    {
+                        message: 'email is not registered',
+                        context: {
+                            key: 'email',
+                            value: data.email,
+                        },
+                    },
+                ]),
+            );
+        });
+
+        it('should throw an error if email is already verified', async () => {
+            const data = {
+                email: 'test1@mail.com',
+                otp: '999999',
+            };
+
+            await expect(verifyOTP(data)).rejects.toThrow(
+                new HttpRequestError(409, 'Resource conflict', [
+                    {
+                        message: 'email is already verified',
                         context: {
                             key: 'email',
                             value: data.email,
